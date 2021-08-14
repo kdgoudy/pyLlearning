75e8dcb5b9610b8b02d9451e8e0cfaa2bc467a23
Koki <kgoudy64@gmail.com>
2021-08-14 09:28:09 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,29 @@
+#Creating a dictionary
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+#Retrieving items from the dictionary
+#print(programming_dictionary["Bug"])
+
+#Adding new values to the dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+
+print(programming_dictionary)
+
+#Creat an empty dictionary 
+#empty_dictionary = {}
+
+#Wipe an existing dictionary 
+#programming_dictionary = {}
+#print(programming_dictionary)
+
+#Edit an item in the dictionary 
+#programming_dictionary["Bug"] = "A moth in your computer"
+
+
+
 #Creating a dictionary using the months of the year
 monthConversions = {
   "Jan": "January",
