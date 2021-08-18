9122039351c89abfeaff5733dc8274054da50b65
Koki <kgoudy64@gmail.com>
2021-08-18 13:56:11 -0500

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,18 @@
+from time import *
+
+start_timer = time()
+struct = localtime(start_timer)
+
+print('Starting Countdown At: ', strftime('%X', struct))
+
+i = 15
+while i > -1:
+  print(i)
+  i -= 1
+  sleep(1)
+
+end_timer = time()
+difference = round(end_timer - start_timer)
+print("Runtime: ", difference, "Seconds")
+
+
